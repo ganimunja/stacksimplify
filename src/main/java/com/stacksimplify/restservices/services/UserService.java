eeb6ec3fba2ab93c29bdf440c775f0c277887bc2
ganimunja <retek.gangadhar@gmail.com>
2022-05-12 22:18:04 +0530

    JPA BASE REST SERVICES DEVELOPMENT COMPLETED

---
+++ src/main/java/com/stacksimplify/restservices/services/UserService.java
@@ -0,0 +1,57 @@
+package com.stacksimplify.restservices.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Service;
+
+import com.stacksimplify.restservices.entities.User;
+import com.stacksimplify.restservices.repositories.UserRepository;
+
+@Service
+public class UserService {
+ 
+	@Autowired
+	private UserRepository userRepository;
+	
+	public List<User> getAllUsers()
+	{
+		return userRepository.findAll();
+	}
+	
+	public User createUser(User user)
+	{
+		return userRepository.save(user);
+		
+	}
+	
+	public Optional<User> getUserById(Long id)
+	{
+	Optional<User> user=	userRepository.findById(id);
+	
+	return user;
+	}
+	
+	public User updateUserId(long id,User user)
+	{
+		user.setId(id);
+		return userRepository.save(user);
+	}
+
+	public void deleteUserById(Long id) {
+		// TODO Auto-generated method stub
+	if(userRepository.findById(id).isPresent())
+	{
+		 userRepository.deleteById(id);
+	}
+	}
+	
+	
+	public User getUserByUserName(String username)
+	{
+	return  userRepository.findByUsername(username);
+	}
+	
+}
