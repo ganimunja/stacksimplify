29e6cf2b613fdb568369e04e90926c564a627393
ganimunja <retek.gangadhar@gmail.com>
2022-05-17 18:33:52 +0530

    exception handling commmit

---
+++ src/main/java/com/stacksimplify/restservices/exceptions/UserNameNotFoundException.java
@@ -0,0 +1,13 @@
+package com.stacksimplify.restservices.exceptions;
+
+public class UserNameNotFoundException extends Exception {
+
+	
+
+	public UserNameNotFoundException(String message) {
+		// TODO Auto-generated constructor stub
+		super(message);
+	}
+	
+
+}
